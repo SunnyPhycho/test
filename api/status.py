6064992492750505178a7ef52aeb5968f064198d
SunnyPhycho <115911879+SunnyPhycho@users.noreply.github.com>
2025-12-31 17:02:00 +0900

    Update status.py

--- api/status.py
+++ api/status.py
@@ -108,7 +108,7 @@ class handler(BaseHTTPRequestHandler):
         
         current_dir = os.path.dirname(os.path.abspath(__file__))
         image_path = os.path.join(current_dir, config['file'])
-        font_path = os.path.join(current_dir, 'font.ttf')
+        font_path = os.path.join(current_dir, 'Diphy.ttf')
 
         if os.path.exists(image_path):
             img = Image.open(image_path).convert("RGBA")
